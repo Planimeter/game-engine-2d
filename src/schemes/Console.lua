b18bb120c1a2c5bfea42c7253a84a85eb776dcb8
Manolis Vrondakis <manolisvrondakis@gmail.com>
2015-06-16 19:26:34 +0100

    Added selectionColor Scheme

--- src/schemes/Console.lua
+++ src/schemes/Console.lua
@@ -10,6 +10,7 @@ console.textbox      = {
     backgroundColor  = color(  35,  35,  36, 0.66 * 255 ),
     outlineColor     = color( 104, 106, 107, 0.66 * 255 ),
     textColor        = color( 104, 106, 107,        255 ),
+    selectionColor   = color(   0,   0,   0, 0.20 *  50 ),
 
     mouseover        = {
         outlineColor = color( 163, 126,  71, 0.42 * 255 ),
