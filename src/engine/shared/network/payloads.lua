aec0c999d347ab93994c4839afa781646b2585fb
Andrew McWatters <mcwattersandrew@gmail.com>
2015-10-10 20:05:10 -0700

    Updated Grid to build 1142

--- src/engine/shared/network/payloads.lua
+++ src/engine/shared/network/payloads.lua
@@ -8,101 +8,101 @@ payload.structs = {}
 local structs   = payload.structs
 
 structs[ "authenticate" ] = {
-	keys = {
-		{ name = "ticket",         type = "string" }
-	}
+    keys = {
+        { name = "ticket",         type = "string" }
+    }
 }
 
 structs[ "kick" ] = {
-	keys = {
-		{ name = "message",        type = "string" }
-	}
+    keys = {
+        { name = "message",        type = "string" }
+    }
 }
 
 structs[ "serverInfo" ] = {
-	keys = {
-		{ name = "region",         type = "string" }
-	}
+    keys = {
+        { name = "region",         type = "string" }
+    }
 }
 
 -- if ( _G._VADVENTURE ) then
-	table.insert( structs[ "serverInfo" ].keys,
-		{ name = "save",           type = "string" }
-	)
+    table.insert( structs[ "serverInfo" ].keys,
+        { name = "save",           type = "string" }
+    )
 -- end
 
 structs[ "download" ] = {
-	keys = {
-		{ name = "filename",       type = "string" }
-	}
+    keys = {
+        { name = "filename",       type = "string" }
+    }
 }
 
 structs[ "upload" ] = {
-	keys = {
-		{ name = "filename",       type = "string" },
-		{ name = "file",           type = "string" }
-	}
+    keys = {
+        { name = "filename",       type = "string" },
+        { name = "file",           type = "string" }
+    }
 }
 
 structs[ "clientInfo" ] = {
-	keys = {
-		{ name = "viewportWidth",  type = "number" },
-		{ name = "viewportHeight", type = "number" }
-	}
+    keys = {
+        { name = "viewportWidth",  type = "number" },
+        { name = "viewportHeight", type = "number" }
+    }
 }
 
 structs[ "entitySpawned" ] = {
-	keys = {
-		{ name = "classname",      type = "string" },
-		{ name = "entIndex",       type = "number" },
-		{ name = "networkVars",    type = "typelenvalues" }
-	}
+    keys = {
+        { name = "classname",      type = "string" },
+        { name = "entIndex",       type = "number" },
+        { name = "networkVars",    type = "typelenvalues" }
+    }
 }
 
 structs[ "playerInitialized" ] = {
-	keys = {
-		{ name = "entIndex",       type = "number" },
-		{ name = "id",             type = "number" }
-	}
+    keys = {
+        { name = "entIndex",       type = "number" },
+        { name = "id",             type = "number" }
+    }
 }
 
 structs[ "entityRemoved" ] = {
-	keys = {
-		{ name = "entIndex",       type = "number" },
-	}
+    keys = {
+        { name = "entIndex",       type = "number" },
+    }
 }
 
 structs[ "chat" ] = {
-	keys = {
-		{ name = "entIndex",       type = "number" },
-		{ name = "message",        type = "string" }
-	}
+    keys = {
+        { name = "entIndex",       type = "number" },
+        { name = "message",        type = "string" }
+    }
 }
 
 structs[ "networkVarChanged" ] = {
-	keys = {
-		{ name = "entIndex",       type = "number" },
-		{ name = "networkVars",    type = "typelenvalues" }
-	}
+    keys = {
+        { name = "entIndex",       type = "number" },
+        { name = "networkVars",    type = "typelenvalues" }
+    }
 }
 
 structs[ "concommand" ] = {
-	keys = {
-		{ name = "name",           type = "string" },
-		{ name = "argString",      type = "string" }
-	}
+    keys = {
+        { name = "name",           type = "string" },
+        { name = "argString",      type = "string" }
+    }
 }
 
 structs[ "playerMove" ] = {
-	keys = {
-		{ name = "position",       type = "vector" },
-	}
+    keys = {
+        { name = "position",       type = "vector" },
+    }
 }
 
 -- if ( _G._VADVENTURE ) then
-	structs[ "playerPickup" ] = {
-		keys = {
-			{ name = "item",       type = "number" },
-		}
-	}
+    structs[ "playerPickup" ] = {
+        keys = {
+            { name = "item",       type = "number" },
+        }
+    }
 -- end
