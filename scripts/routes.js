d9583f0049c57a4bcc851762d740bd82e6012327
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-12 13:24:02 -0700

    Updated handlers

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,19 +1,14 @@
 route
   .when('/', {
     templateUrl: 'views/main.html',
-    handler: function() {
-      hljs.configure({ languages: ['Lua'] });
-      var elements = document.querySelectorAll('pre code');
-      Array.prototype.forEach.call(elements, function(block){
-        hljs.highlightBlock(block);
-      });
-    }
+    handler: Main
   })
   .when('/api', {
     redirectTo: '/api/Home'
   })
   .when('/api/:article', {
     templateUrl: 'views/api.article.html',
+    handler: Api
   })
   .when('/404', {
     templateUrl: 'views/404.html'
