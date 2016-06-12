7904f462d3dceb009e7dfdbd8654e60cc376e8a3
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-12 08:40:37 -0700

    Added legacy views

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,4 +1,14 @@
 route
   .when('/', {
-    templateUrl: 'views/about.html'
-  });
+    templateUrl: 'views/main.html'
+  })
+  .when('/api', {
+    templateUrl: 'views/api.html',
+  })
+  .when('/api/:article', {
+    templateUrl: 'views/api.article.html',
+  })
+  .when('/404', {
+    templateUrl: 'views/404.html'
+  })
+  .otherwise('/404');
