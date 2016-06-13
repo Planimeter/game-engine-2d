261770d3ca53d8ff95f1bb0605f55867fba87a22
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-13 07:25:12 -0700

    Updated grid and nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,11 @@
 (function() {
+  function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
 
+  ready(nav);
 })();
