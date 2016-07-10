ff79cbbcad4007ca7da9f43b9281455257190203
Andrew McWatters <mcwattersandrew@gmail.com>
2016-07-10 10:50:41 -0700

    Code drop

--- src/game/client/gui/hudhealth.lua
+++ src/game/client/gui/hudhealth.lua
@@ -8,10 +8,15 @@ class "hudhealth" ( gui.panel )
 
 function hudhealth:hudhealth( parent )
 	gui.panel.panel( self, parent, "HUD Health" )
-	self.width = point( 64 )
-	self.height = point( 64 )
+	self.width = point( 102 )
 
 	self:setScheme( "Default" )
+	local font = self:getScheme( "entityFont" )
+	self.height = font:getHeight()
+	font = self:getScheme( "font" )
+	self.height = self.height + font:getHeight()
+
+	self:invalidateLayout()
 end
 
 function hudhealth:draw()
@@ -21,29 +26,44 @@ function hudhealth:draw()
 	gui.panel.draw( self )
 end
 
-function hudhealth:drawLabel()
-end
-
 function hudhealth:drawHealth()
 	local property = "hudmoveindicator.textColor"
 	graphics.setColor( self:getScheme( property ) )
 	local font = self:getScheme( "entityFont" )
 	graphics.setFont( font )
 	local health = localplayer:getNetworkVar( "health" )
-	local margin = gui.scale( 96 )
 	graphics.print(
 		health, -- text
-		margin, -- x
-		margin, -- y
-		0,      -- r
-		1,      -- sx
-		1,      -- sy
-		0,      -- ox
-		0,      -- oy
-		0,      -- kx
-		0,      -- ky
-		2       -- tracking
+		0, -- x
+		0, -- y
+		0, -- r
+		1, -- sx
+		1, -- sy
+		0, -- ox
+		0, -- oy
+		0, -- kx
+		0, -- ky
+		2  -- tracking
 	)
+end
+
+function hudhealth:drawLabel()
+	local property = "hudmoveindicator.smallTextColor"
+	graphics.setColor( self:getScheme( property ) )
+	local font = self:getScheme( "entityFont" )
+	local lineHeight = font:getHeight()
+	font = self:getScheme( "font" )
+	graphics.setFont( font )
+	graphics.print( "Health", 0, lineHeight )
+end
+
+function hudhealth:invalidateLayout()
+	local margin = gui.scale( 96 )
+	local viewportHeight = graphics.getViewportHeight()
+	local height = self:getHeight()
+	self:setPos( margin, viewportHeight - margin - height )
+
+	gui.panel.invalidateLayout( self )
 end
 
 gui.register( hudhealth, "hudhealth" )
