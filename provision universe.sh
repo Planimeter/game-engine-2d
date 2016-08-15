789be6a3ad76678c0bad888d825cb1bf09f16c68
Andrew McWatters <mcwattersandrew@gmail.com>
2016-08-15 14:36:33 -0700

    Removed scripts for love-grid update

--- provision universe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get upgrade -y
-apt-get install python-software-properties software-properties-common -y
-add-apt-repository ppa:bartbes/love-stable
-apt-get update
-apt-get install love -y
