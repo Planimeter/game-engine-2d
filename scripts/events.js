3f4d8c0edb30f55cbc7736f105435794f07743e1
Andrew McWatters <mcwattersandrew@gmail.com>
2016-12-08 19:31:19 -0700

    Remove event object logging

--- scripts/events.js
+++ scripts/events.js
@@ -3,27 +3,22 @@
 var target = window;
 target.addEventListener('routechange', function(event) {
   console.log('routechange');
-  console.log(event);
 });
 
 target = document.querySelector('[data-view]');
 target.addEventListener('viewcontentloaded', function(event) {
   console.log('viewcontentloaded');
-  console.log(event);
 });
 
 target = document.body;
 target.addEventListener('includecontentrequested', function(event) {
   console.log('includecontentrequested');
-  console.log(event);
 });
 
 target.addEventListener('includecontentloaded', function(event) {
   console.log('includecontentloaded');
-  console.log(event);
 });
 
 target.addEventListener('includecontenterror', function(event) {
   console.log('includecontenterror');
-  console.log(event);
 });
