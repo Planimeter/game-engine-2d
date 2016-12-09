f238bd5a78c03b7d4330679834696b8beaf4eb70
Andrew McWatters <mcwattersandrew@gmail.com>
2016-12-08 19:40:43 -0700

    Add MutationObserver events

--- scripts/events.js
+++ scripts/events.js
@@ -10,6 +10,26 @@ target.addEventListener('viewcontentloaded', function(event) {
   console.log('viewcontentloaded');
 });
 
+function update(node) {
+  if (node.dataset && node.dataset.include) {
+    console.log('childList mutation');
+    return;
+  }
+
+  var childNodes = node.childNodes;
+  for (var i = 0; i < childNodes.length; i++) {
+    update(childNodes[i]);
+  }
+}
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    Array.prototype.forEach.call(mutation.addedNodes, update);
+  });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
 target = document.body;
 target.addEventListener('includecontentrequested', function(event) {
   console.log('includecontentrequested');
