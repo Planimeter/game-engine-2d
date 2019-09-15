3b25891ee7d4d87f9f1fb64d048b05b988f5a684
Andrew McWatters <andrewmcwatters@Andrews-iMac.local>
2019-09-15 12:17:59 -0700

    Add roadmap route

--- scripts/routes.js
+++ scripts/routes.js
@@ -19,6 +19,9 @@ route
     templateUrl: 'views/api.html',
     handler: Article
   })
+  .when('/roadmap', {
+    templateUrl: 'views/roadmap.html'
+  })
   .when('/404', {
     templateUrl: 'views/404.html'
   })
